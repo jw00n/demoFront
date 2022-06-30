e64c807d33d35a7751655c1f795ca350364a274d
user03 <user03@DESKTOP-2JJDG7F>
2022-06-30 10:27:38 +0900

    test 1

---
+++ demofront/scripts/profile.sh
@@ -0,0 +1,35 @@
+# 쉬고있는 profile 찾기
+
+function find_idle_profile(){
+    
+    RESPONSE_CODE=$(curl -L -s -o /dev/null -w "%{http_code}" https://demofront.ddns.net/profile)
+ 
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+    then
+        CURRENT_PROFILE=prof2
+    else
+        CURRENT_PROFILE=$(curl -L -s https://demofront.ddns.net/profile)
+    fi
+
+    if [ ${CURRENT_PROFILE} == prof1 ]
+    then
+      IDLE_PROFILE=prof2
+    else
+      IDLE_PROFILE=prof1
+    fi
+
+    echo "${IDLE_PROFILE}"
+}
+
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port()
+{
+    IDLE_PROFILE=$(find_idle_profile)
+
+    if [ ${IDLE_PROFILE} == prof1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
+}
